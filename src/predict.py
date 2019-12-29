f8e45d39e3c7c4e336f0bcce5612176d557873b1
loney <geo.giatrakos@gmail.com>
2019-12-29 19:57:16 +0200

    apply predict on data

--- src/predict.py
+++ src/predict.py
@@ -17,7 +17,6 @@ def mean_absolute_percentage_error(y_true, y_pred):
 def mean_square_root(y_true,y_pred) :
     return (np.square(y_pred - y_true)).mean(axis=None)
 
-
 def predict(input,model):
     y_prob = model.predict(input)
     return y_prob
@@ -25,3 +24,10 @@ def predict(input,model):
 
 model = load_model('../data/WindDenseNN.h5',compile=False)
 model.summary()
+
+
+data = pd.read_csv(sys.argv[2],header=None)
+
+r = data.apply(lambda x : (x[0],predict(x[1:].values.reshape(1,-1),model)),axis=1)
+#r = data.apply(lambda x : (x[0],predict(list((x[1:].values)),model)),axis=1)
+print(r)
